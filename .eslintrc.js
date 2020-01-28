1758c7d0a774965c3b524636eb6faa1ca6e692da
Robert Pearce <me@robertwpearce.com>
2020-01-28 17:07:06 -0500

    bump eslint-config-prettier, lint-staged, rollup, rollup-plugin-postcss, react-use; make sure to lint eslintrc and prettierrc, too

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,22 +16,17 @@ module.exports = {
     ecmaVersion: 2019,
     sourceType: 'module'
   },
-  plugins: [
-    'css-modules',
-    'jsx-a11y',
-    'react',
-    'react-hooks'
-  ],
+  plugins: ['css-modules', 'jsx-a11y', 'react', 'react-hooks'],
   rules: {
-    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    //indent: ['error', 2, { SwitchCase: 1 }],
     'jsx-quotes': ['error', 'prefer-double'],
     'jsx-a11y/no-onchange': 0,
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
-    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
     'react-hooks/exhaustive-deps': 'warn',
     'react-hooks/rules-of-hooks': 'error',
-    'semi': ['error', 'never']
+    semi: ['error', 'never']
   },
   settings: {
     react: {
