88aa8ce7fe05a91f692f99e28eedf625ca902b12
Robert Pearce <me@robertwpearce.com>
2020-03-04 09:28:39 -0500

    babel and version

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
 const presets = ['@babel/react', ['@babel/env', { loose: true }]]
 
+const testPresets = [
+  '@babel/react',
+  ['@babel/env', { loose: true, targets: { node: 'current' } }]
+]
+
 const plugins = [['transform-react-remove-prop-types', { mode: 'wrap' }]]
 
-module.exports = { presets, plugins }
-
-//{
-//  targets: { node: 'current' }
-//}
+module.exports = {
+  env: { test: { presets: testPresets } },
+  presets,
+  plugins
+}
