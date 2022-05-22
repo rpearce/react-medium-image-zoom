a0d2c79a7e0262f6e88b602aeaeaec3ac5918f24
Robert W. Pearce <me@robertwpearce.com>
2022-05-22 13:48:40 -0400

    Chore: eslint updates; use standard config

--- testSetup.js
+++ testSetup.js
@@ -1,11 +1,11 @@
 require('@testing-library/jest-dom/extend-expect')
 
 // https://stackoverflow.com/a/68468204/680394
-const { TextEncoder, TextDecoder } = require('util')
+const { TextEncoder, TextDecoder } = require('util') // eslint-disable-line @typescript-eslint/no-var-requires
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
 
 Object.defineProperty(window, 'scroll', {
   value: () => undefined,
-  writable: true
+  writable: true,
 })
