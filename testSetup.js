ec1cb5c362583d42ac86993eec77701ef20215bc
Robert W. Pearce <me@robertwpearce.com>
2022-07-21 19:34:15 -0400

    Add ResizeObserver & img.decode() to testSetup

--- testSetup.js
+++ testSetup.js
@@ -9,3 +9,15 @@ Object.defineProperty(window, 'scroll', {
   value: () => undefined,
   writable: true,
 })
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}))
+
+Object.defineProperty(global.Image.prototype, 'decode', {
+  get() {
+    return () => Promise.resolve()
+  },
+})
