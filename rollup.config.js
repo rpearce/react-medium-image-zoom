76dd774a58d8d818e2ec3dd6aeae83b5f3edc19a
Robert W. Pearce <me@robertwpearce.com>
2022-10-15 13:48:07 -0400

    Upgrade dev deps, fix importing JSON file, and improve playwright test

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import ts from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-import pkg from './package.json'
+import pkg from './package.json' assert { type: 'json' }
 
 export default [
   {
