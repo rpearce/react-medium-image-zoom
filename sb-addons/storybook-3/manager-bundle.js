a639139ead0c770abfadf3191c253b2c5b443b5b
rpearce <rpearce@users.noreply.github.com>
2023-05-04 01:16:02 +0000

    deploy: 7114ed95212fa37cd6ff07e14d9d0de89857e2e9

---
+++ sb-addons/storybook-3/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var a=__STORYBOOKADDONS__,{addons:O,types:d,mockChannel:m}=__STORYBOOKADDONS__;O.setConfig({panelPosition:"right"});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
