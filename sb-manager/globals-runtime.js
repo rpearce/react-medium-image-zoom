0b0ed7afcd33e30bb753bd2041817a875f508d2d
rpearce <rpearce@users.noreply.github.com>
2026-01-28 19:16:24 +0000

    deploy: 339175c3a60b335628c906a0fa186570c30da1d4

--- sb-manager/globals-runtime.js
+++ sb-manager/globals-runtime.js
@@ -62174,7 +62174,7 @@ init_dist();
 var import_memoizerific8 = __toESM(require_memoizerific(), 1), import_semver = __toESM(require_semver2(), 1);
 
 // src/manager-api/version.ts
-var version = "10.2.0";
+var version = "10.2.1";
 
 // src/manager-api/modules/versions.ts
 var { VERSIONCHECK } = scope, getVersionCheckData = (0, import_memoizerific8.default)(1)(() => {
